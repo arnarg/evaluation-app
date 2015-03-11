8e5227aa9e8b42bf0fcb68250e0cf9e5d43117d4
Þórdís Jóna <thordisj12@ru.is>
2015-03-11 13:05:20 +0000

    Grunt dependencies

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function (grunt) {
+	var taskConfig = {
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			options: {
+				curly:  true,
+				immed:  true,
+				newcap: true,
+				noarg:  true,
+				sub:    true,
+				boss:   true,
+				eqnull: true,
+				node:   true,
+				undef:  true,
+				browser: true,	// to allow document
+				globals: {
+					_:       false,
+					jQuery:  false,
+					angular: false,
+					moment:  false,
+					console: false,
+					$:       false,
+					io:      false,
+					toastr: true,
+					ChatClient: true // global variable for the angular module
+				}
+			},
+			src: ['src/js/app.js', 'src/js/**/*.js'],
+			gruntfile: ['Gruntfile.js']
+		},
+		concat: {
+			options: {
+				separator: '\n'
+			},
+			dist: {
+				src: ['src/js/app.js', 'src/js/**/*.js'],
+				dest: 'src/dest/app.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: {
+					'public/js/app.js': ['src/dest/app.js']
+				}
+			}
+		}
+	};
+
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.initConfig(taskConfig);
+};
