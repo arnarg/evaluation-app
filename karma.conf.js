b79b18ecec740f6aaf025c1cc2ebaf4f251e360d
Þórdís Jóna <thordisj12@ru.is>
2015-03-11 16:17:54 +0000

    Code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -25,12 +25,13 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+    	'src/js/*.js': ['coverage']
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
     // web server port
     port: 9876,
@@ -48,6 +49,10 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
+
+    coverageReporter: {
+    	'dir': 'coverage/'
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
