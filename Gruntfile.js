143d879c511df52bfef7309f330257202c094777
Arnar Gauti Ingason <granra@gmail.com>
2015-03-11 14:23:48 +0000

    Yeah

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,5 +57,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-karma');
 	grunt.initConfig(taskConfig);
 };
