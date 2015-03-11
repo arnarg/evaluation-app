774b07f60548a453ae810180ed26dd7a027237c4
Atli Guðlaugsson <aaatttllliii@gmail.com>
2015-03-11 16:08:53 +0000

    first factory and apes happening

--- src/js/controllers/logincontroller.js
+++ src/js/controllers/logincontroller.js
@@ -1,14 +1,19 @@
 angular.module("evalApp").controller("loginController",
-["$scope", "$rootScope", "$state", 
-function ($scope, $rootScope, $state){
+["$scope", "$rootScope", "$state", "LoginResource", 
+function ($scope, $rootScope, $state, LoginResource){
 	$scope.errorMessage = "";
 	$scope.nickname = "";
 	$scope.password = "";
 	$scope.message = "Hello from Login";
 
 	$scope.login = function(){
-		if($scope.nicname === "" || $scope.password === ""){
+		if($scope.nickname === "" || $scope.password === ""){
 			$scope.errorMessage = "Please input nickname and password";
+		}
+		else{
+			LoginResource.login({user: $scope.nickname, pass: $scope.password}).then(function(data){
+				console.log(data);
+			});
 		}
 	};
 
