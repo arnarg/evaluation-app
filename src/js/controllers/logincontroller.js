f33c266145216e7e4cb27e0721e3d501ee12ed7a
Atli Guðlaugsson <aaatttllliii@gmail.com>
2015-03-11 13:41:46 +0000

    changed index and added controllers directory

---
+++ src/js/controllers/logincontroller.js
@@ -0,0 +1,14 @@
+angular.module("evalApp").controller("loginController",
+["$scope", "$rootScope", "$state", 
+function ($scope, $rootScope, $state){
+	$scope.errorMessage = "";
+	$scope.nickname = "";
+	$scope.password = "";
+	$scope.message = "Hello from Login";
+
+	$scope.login = function(){
+		if($scope.nicname === "" || $scope.password === ""){
+			$scope.errorMessage = "Please input nickname and password";
+		}
+	}
+}]);
