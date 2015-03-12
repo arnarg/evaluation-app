242ad4f068b3122f32f8ec8c4fb89e9b518797aa
Atli Guðlaugsson <aaatttllliii@gmail.com>
2015-03-12 16:32:19 +0000

    lots of stuff

---
+++ src/js/controllers/templatecontroller.js
@@ -0,0 +1,6 @@
+angular.module("evalApp").controller("templateController",
+["$scope", "$rootScope", "$state", "userData", "TemplatesResource", 
+function ($scope, $rootScope, $state, userData, TemplatesResource){
+	$scope.errorMessage = "";
+
+}]);
