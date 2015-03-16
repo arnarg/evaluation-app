bf7809227cd8c83f3e60f3b97f885ed9b00ea6cc
Arnar Gauti Ingason <granra@gmail.com>
2015-03-16 16:02:30 +0000

    Beginning modal window on add template

--- src/js/controllers/templatecontroller.js
+++ src/js/controllers/templatecontroller.js
@@ -1,6 +1,20 @@
 angular.module("evalApp").controller("templateController",
-["$scope", "$rootScope", "$state", "userData", "TemplatesResource", 
-function ($scope, $rootScope, $state, userData, TemplatesResource){
+["$scope", "$rootScope", "$state", "userData", "TemplatesResource", "$modal",
+function ($scope, $rootScope, $state, userData, TemplatesResource, $modal){
 	$scope.errorMessage = "";
+	$scope.templateTitle = "";
+	$scope.questions = [];
 
+	$scope.addQuestion = function() {
+		var modal = $modal.open({
+			templateUrl: "views/addtemplate.html",
+			controller: "templateModalController"
+		});
+
+		modal.result.then(function(data) {
+			console.log(data);
+		}, function() {
+			console.log("Dismissed");
+		});
+	};
 }]);
