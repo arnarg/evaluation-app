00cb45c2c711456b85f4884b1153577b4e6863b8
Atli Guðlaugsson <aaatttllliii@gmail.com>
2015-03-18 14:12:17 +0000

    Admin view eval results can switch between courses

--- src/js/controllers/EvaluationAdminController.js
+++ src/js/controllers/EvaluationAdminController.js
@@ -5,16 +5,7 @@ function ($scope, $rootScope, $state, $stateParams, EvaluationsResource, userDat
 	$scope.titleEN = "";
 	$scope.courses = [];
 	$scope.selectedOption = { };
-	var nothing = [];
-	var answers1 = [];
-	answers1.push({ AnswerText: "Not cool", Count: 10 });
-	answers1.push({ AnswerText: "Okay", Count: 5 });
-	answers1.push({ AnswerText: "Was cool", Count: 30 });
-	var questions1 = [];
-	questions1.push({ ID: 1, Text: "Describe course", type: "text", TextResults: "It was cool", OptionsResult: nothing});
-	questions1.push({ ID: 2, Text: "How cool was teacher?", type: "single", TextResults: "", OptionsResult: answers1 });
-	questions1.push({ ID: 3, Text: "How cool was course?", type: "multiple", TextResults: "", OptionsResult: answers1 });
-	
+
 	EvaluationsResource.getEvaluation(userData.token, $stateParams.id).then(function(data){
 		console.log(data);
 		$scope.title = data.data.TemplateTitle;
@@ -30,11 +21,13 @@ function ($scope, $rootScope, $state, $stateParams, EvaluationsResource, userDat
 	});
 
 	$scope.getCourseResults = function(id){
+		console.log(id);
 		$scope.courses.forEach(function(course){
 			if(course.CourseID === id){
 				$scope.selectedOption = course;
 			}
 		});
+		console.log($scope.selectedOption);
 		fillScopeData();
 	};
 
